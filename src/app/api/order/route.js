b78cec303d51d99e472bb735473ba0c51f75d7a5
Antoine Pineau <a.pineau@eurelis.com>
2024-08-25 14:35:59 +0200

    changed functions

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,8 +1,9 @@
+import { NextResponse } from 'next/server';
 import { getAccessToken, createOrder } from '../../utils/helloasso';
 
 export async function POST(req, res) {
   const token = await getAccessToken();
   const tickets = await createOrder(token, req.body);
   
-  res.status(200).json(tickets);
+  return NextResponse.json(tickets);
 }
--- src/app/api/tickets/route.js
+++ src/app/api/tickets/route.js
@@ -1,8 +1,9 @@
+import { NextResponse } from 'next/server';
 import { getAccessToken, getAvailableTickets } from '../../utils/helloasso';
 
 export async function GET(req, res) {
   const token = await getAccessToken();
   const tickets = await getAvailableTickets(token);
   
-  res.status(200).json(tickets);
+  return NextResponse.json(tickets);
 }
