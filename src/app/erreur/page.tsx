ede50945b3dddb3eab572e009bae6d072db43665
Antoine Pineau <a.pineau@eurelis.com>
2024-08-27 00:13:40 +0200

    try to make the cart working

--- src/app/erreur/page.tsx
+++ src/app/erreur/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Error() {
   return (
     <div>
-      <h1>Une erreur s'est produite</h1>
+      <h1>Une erreur s&apos;est produite</h1>
     </div>
   );
 }
