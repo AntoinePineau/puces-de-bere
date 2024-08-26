4b6eb4cc59f424281e945f7496eca23da77e7806
Antoine Pineau <a.pineau@eurelis.com>
2024-08-26 08:00:41 +0200

    try to make the cart working

---
+++ src/app/panier/page.tsx
@@ -0,0 +1,11 @@
+// /src/app/panier/page.tsx
+import Panier from '../../components/Panier';
+
+export default function PanierPage() {
+  return (
+    <div>
+      <h1>Mon Panier</h1>
+      <Panier />
+    </div>
+  );
+}
