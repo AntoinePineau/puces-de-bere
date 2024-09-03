50c35e5e03ce5fb5ecf1feac532a3cd15834cfb4
Antoine Pineau <a.pineau@eurelis.com>
2024-09-03 14:41:11 +0200

    change seats

--- src/atoms/getSeats.ts
+++ src/atoms/getSeats.ts
@@ -142,12 +142,16 @@ export async function getSeats():Promise<Seat[]> {
   xy = addSeat(seats, `R25`, seatWidth, seatHeight, xy.x, xy.y-seatHeight-1, descAvecInterieur, prixAvecInterieur, existingSeatIds);
   xy = addSeat(seats, `R26`, seatHeight, seatWidth, xy.x, xy.y-seatHeight*3/2, descAvecInterieur, prixAvecInterieur, existingSeatIds); 
   xy = addSeat(seats, `R27`, seatHeight, seatWidth, xy.x, xy.y-seatWidth-1, descSansInterieur, prixSansInterieur, existingSeatIds);  
-  xy = addSeat(seats, `R37`, seatHeight, seatWidth, 3784, 920, descSansInterieur, prixSansInterieur, existingSeatIds);
-  for(var i=36;i>=28;i--) { 
+  xy = addSeat(seats, `R36`, seatHeight, seatWidth, 3800, 920, descSansInterieur, prixSansInterieur, existingSeatIds);
+  for(var i=35;i>=28;i--) { 
     xy = addSeat(seats, `R${i}`, seatHeight, seatWidth, xy.x, xy.y-seatWidth-1, descSansInterieur, prixSansInterieur, existingSeatIds);
   }
-  xy = addSeat(seats, `R39`, seatWidth, seatHeight, 3578, 920-seatHeight/2, descSansInterieur, prixSansInterieur, existingSeatIds);
-  xy = addSeat(seats, `R38`, seatWidth, seatHeight, xy.x, xy.y-seatHeight-1, descSansInterieur, prixSansInterieur, existingSeatIds);
+  xy = addSeat(seats, `R38`, seatWidth, seatHeight, 3584, 920-seatHeight/2-5, descSansInterieur, prixSansInterieur, existingSeatIds);
+  xy = addSeat(seats, `R37`, seatWidth, seatHeight, xy.x, xy.y-seatHeight-1, descSansInterieur, prixSansInterieur, existingSeatIds);
+  xy = addSeat(seats, `R46`, seatHeight, seatWidth, 2900, 920-seatHeight/2-5, descSansInterieur, prixSansInterieur, existingSeatIds);
+  for(var i=45;i>=39;i--) { 
+    xy = addSeat(seats, `R${i}`, seatHeight, seatWidth, xy.x, xy.y-seatWidth-1, descSansInterieur, prixSansInterieur, existingSeatIds);
+  }
 
   // Line X
   for(var i=1;i<=33;i++) { 
