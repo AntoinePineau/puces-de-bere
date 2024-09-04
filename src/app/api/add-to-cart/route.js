c83d5afd32850cf948e8bf918d995358af56d6c1
Antoine Pineau <a.pineau@eurelis.com>
2024-09-04 16:49:27 +0200

    changed access token

--- src/app/api/add-to-cart/route.js
+++ src/app/api/add-to-cart/route.js
@@ -3,6 +3,7 @@ import { getAccessTokenWithoutAPI, addToCart } from '@/app/utils/helloasso';
 
 export async function POST(req, res) {
   const token = await getAccessTokenWithoutAPI();
+  console.log(`token: ${token}`);
   const response = await addToCart(token, req.body);
   return NextResponse.json(response);
 }
