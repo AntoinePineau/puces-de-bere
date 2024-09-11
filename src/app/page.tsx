49433e376d48330f0537ab59a03fee012e912c2f
Antoine Pineau <a.pineau@eurelis.com>
2024-09-11 17:34:06 +0200

    add widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <Panier />
       </aside>
       <section>
-      <iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/rotary-club-chateaubriant/evenements/puces-de-bere/widget-bouton" style="width: 100%; height: 70px; border: none;"></iframe>
+      <iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/rotary-club-chateaubriant/evenements/puces-de-bere/widget-bouton" height={"70px"} width={"100%"} style={{border:"none"}}></iframe>
       </section>
     </main>
   );
