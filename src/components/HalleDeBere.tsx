149333d4ad6ecdd985a56dcf851292e867a26483
Antoine Pineau <a.pineau@eurelis.com>
2024-10-08 06:46:52 +0200

    change halle de bere vide

--- src/components/HalleDeBere.tsx
+++ src/components/HalleDeBere.tsx
@@ -108,7 +108,7 @@ export default function HalleDeBere() {
   }, [seats]);
 
   return (
-    <svg ref={svgRef} id="halledebere" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="350px" viewBox="0 1370 9361.11 3800" preserveAspectRatio="xMidYMid meet" className="cursor-grab border-2" style={{ touchAction: 'manipulation' }}>
+    <svg ref={svgRef} id="halledebere" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="500px" viewBox="0 1370 9361.11 3800" preserveAspectRatio="xMidYMid meet" className="cursor-grab border-2" style={{ touchAction: 'manipulation' }}>
         <g style={{/*transform: 'translate(-660pt, -370pt)'*/}}>
           <HalleDeBereVide/>
         </g>
