af4bdff18f331971de8677ebe2a1b58eb9ba890a
Antoine Pineau <a.pineau@eurelis.com>
2024-10-09 11:27:21 +0200

    change halle de bere vide

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -2,10 +2,11 @@ import { NextResponse } from 'next/server';
 import { getAccessToken, initCheckout } from '../../utils/helloasso';
 
 export async function POST(req, res) {
-  console.log('Body:', req.body);
+  const body = await req.text();
+  console.log('Body:', body);
   const token = await getAccessToken();
   console.log('Token:', token);
-  const checkout = await initCheckout(token, req.body);
+  const checkout = await initCheckout(token, JSON.parse(body));
   console.log('checkout:', checkout);  
   return NextResponse.json(checkout);
 }
