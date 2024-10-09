51d118e19d44c293178797bb80a531faad1b2056
Antoine Pineau <a.pineau@eurelis.com>
2024-10-09 11:04:22 +0200

    change halle de bere vide

--- src/app/utils/helloasso.js
+++ src/app/utils/helloasso.js
@@ -107,7 +107,7 @@ export async function initCheckout(token, orderDetails) {
       'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json',
     },
-    body: orderDetails
+    body: JSON.stringify(orderDetails)
   });
 
   const data = await response.json();
