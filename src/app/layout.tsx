bd0dd7580604dec783fa74726410d836355abb49
Antoine Pineau <a.pineau@eurelis.com>
2024-10-15 13:38:00 +0200

    update l&f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
         <CartProvider>
           <main className="flex flex-col items-center justify-between">
             <h1>Les Puces de Béré 2025</h1>
-            <div className="inline-flex" style={{ margin: '0 1em 1em' }}>
+            <div className="inline-flex" style={{ margin: '0 1em 1em 1em' }}>
               <img src="/logo-rotary-chateaubriant.png" alt="Logo du Rotary Club de Châteaubriant" title="Rotary Club de Châteaubriant" className="max-h-16" />
               <div className="border-r border-[rgb(190,196,201)] h-16 mx-2" /> {/* Vertical line */}
               <img src="/la-magie-du-rotary.png" alt="Thème de l'année 2024-2025" title="La Magie du Rotary" className="max-h-16" />
