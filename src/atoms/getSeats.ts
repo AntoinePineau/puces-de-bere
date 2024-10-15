d71fde5179bf0858bb18b98acfa04a3ff831418f
Antoine Pineau <a.pineau@eurelis.com>
2024-10-15 15:58:09 +0200

    update prices

--- src/atoms/getSeats.ts
+++ src/atoms/getSeats.ts
@@ -112,7 +112,7 @@ export async function getSeats():Promise<Seat[]> {
   // Line S
   xy.x = topLeftX;
   for(var i=1;i<=5;i++) { 
-    xy = addSeat(seats, `S${i}`, true, i==1, xy.x, bottomRightY-seatWidth, descSansInterieur, prixSansInterieur, existingSeatIds);
+    xy = addSeat(seats, `S${i}`, true, i==1, xy.x, bottomRightY-seatWidth, i==1 ? descAvecInterieur : descSansInterieur, i==1 ? prixAvecInterieur : prixSansInterieur, existingSeatIds);
   }
   xy = addSeat(seats, `S6`, true, false, xy.x+seatHeight/2, bottomRightY-seatWidth, descSansInterieur, prixSansInterieur, existingSeatIds);
   xy = addSeat(seats, `S7`, true, false, xy.x, bottomRightY-seatWidth, descSansInterieur, prixSansInterieur, existingSeatIds);
@@ -141,7 +141,7 @@ export async function getSeats():Promise<Seat[]> {
   for(var i=1;i<=5;i++) { 
     xy = addSeat(seats, `R${i}`, true, false, topLeftX+seatWidth+space*3/4+(seatHeight+1)*i, topLeftY-seatHeight-seatWidth-angleL-space, descSansInterieur, prixSansInterieur, existingSeatIds);
   }
-  addSeat(seats, `R6`, true, true, xy.x, xy.y-seatWidth-1, descSansInterieur, prixSansInterieur, existingSeatIds);
+  addSeat(seats, `R6`, true, true, xy.x, xy.y-seatWidth-1, descAvecInterieur, prixAvecInterieur, existingSeatIds);
   addSeat(seats, `R7`, false, false, xy.x+seatWidth/3, xy.y-seatHeight-seatWidth-2, descSansInterieur, prixSansInterieur, existingSeatIds);
   xy = addSeat(seats, `R8`, true, false, xy.x+space*7/4, xy.y-seatHeight*2-seatWidth, descSansInterieur, prixSansInterieur, existingSeatIds);
   xy = addSeat(seats, `R9`, true, false, xy.x, xy.y-seatWidth-1, descSansInterieur, prixSansInterieur, existingSeatIds);
@@ -181,7 +181,7 @@ export async function getSeats():Promise<Seat[]> {
   }
   xy.x = xyN6.x-anglel-1;
   for(var i=56;i>=55;i--) { 
-    xy = addSeat(seats, `R${i}`, true, true, xy.x, xyR46.y-seatWidth-1, descSansInterieur, prixSansInterieur, existingSeatIds);
+    xy = addSeat(seats, `R${i}`, true, true, xy.x, xyR46.y-seatWidth-1, descAvecInterieur, prixAvecInterieur, existingSeatIds);
   }
 
 
