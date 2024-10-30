9af35c3231d08b28702393f0962e8aeafef1ecc5
Antoine Pineau <a.pineau@eurelis.com>
2024-10-30 01:13:56 +0100

    send mail

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -2,7 +2,9 @@ import nodemailer from 'nodemailer';
 import formidable from 'formidable'; 
 
 export async function POST(req, res) {
-  const form = new formidable.IncomingForm();
+  const form = new formidable.IncomingForm({
+    maxFileSize: 25 * 1024 * 1024, // Limite de 25 Mo, ajustez selon vos besoins
+  });
   
   form.parse(req, async (err, fields, files) => {
     if (err) {
